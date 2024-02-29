1af232dfc9f2d94b12fb13995d1c9c673190c346
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2024-02-29 10:44:27 -0800

    -Add Place All Command -Fix Forge Version Check

--- Fabric/src/main/java/corgitaco/corgilib/platform/FabricPlatform.java
+++ Fabric/src/main/java/corgitaco/corgilib/platform/FabricPlatform.java
@@ -1,15 +1,21 @@
 package corgitaco.corgilib.platform;
 
 import com.google.auto.service.AutoService;
+import com.google.common.base.Suppliers;
 import corgitaco.corgilib.network.FabricNetworkHandler;
 import corgitaco.corgilib.network.Packet;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.level.ServerPlayer;
 
 import java.nio.file.Path;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Supplier;
 
 @AutoService(ModPlatform.class)
 public class FabricPlatform implements ModPlatform {
+    private static final Supplier<List<String>> MOD_IDS = Suppliers.memoize(() -> FabricLoader.getInstance().getAllMods().stream().map(modContainer -> modContainer.getMetadata().getId()).toList());
+
 
     @Override
     public String getPlatformName() {
@@ -39,5 +45,10 @@ public class FabricPlatform implements ModPlatform {
     @Override
     public Path configDir() {
         return FabricLoader.getInstance().getConfigDir();
+    }
+
+    @Override
+    public Collection<String> getModIDS() {
+        return MOD_IDS.get();
     }
 }
